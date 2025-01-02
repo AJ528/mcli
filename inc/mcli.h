a17c270f438543ea0e96e01531ab9197d59addd9
ratman528 <adevries528@gmail.com>
2025-01-01 21:06:26 -0600

    initial attempt. May need to just make this an embedded project...

---
+++ inc/mcli.h
@@ -0,0 +1,5 @@
+#ifndef __MCLI_H
+#define __MCLI_H
+
+
+#endif /* __MCLI_H */
---
+++ src/main.c
@@ -0,0 +1,71 @@
+#include <termios.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+static struct termios old, current;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  current = old; /* make new settings same as old settings */
+  current.c_lflag &= ~ICANON; /* disable buffered i/o */
+  if (echo) {
+      current.c_lflag |= ECHO; /* set echo mode */
+  } else {
+      current.c_lflag &= ~ECHO; /* set no echo mode */
+  }
+  tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void) 
+{
+  return getch_(1);
+}
+
+/* Let's test it out */
+int main(void) {
+  char buf[100] = {0};
+  char c = 0;
+  int i = 0;
+  while(c != '\n'){
+    c = getch();
+    putchar((int) c);
+
+    if(i < 99){
+      buf[i++] = c;
+    }
+
+    if(c == '\n'){
+      printf("entered text was: ");
+      for(int j = 0; j < i; j++){
+        putchar((int) buf[j]);
+      }
+    }
+
+  }
+  return 0;
+}
